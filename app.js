29e9862c9b32319bc1b62d2d8b3db579c957f594
shravan <shravan.318@gmail.com>
2019-10-12 13:27:03 +0530

    add loader and imporve UX

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function(e){
+    // hide results
+    document.getElementById("results").style.display = "none";
+    //loader
+    document.getElementById("loading").style.display = "block"
+
+    setTimeout(calculateResults, 2000)
+e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
     console.log('Calculating...');
     // UI Vars
     const amount = document.getElementById('amount');
@@ -24,15 +32,22 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        // hide laoder
+        document.getElementById("loading").style.display = "none";
+        // show result
+        document.getElementById("results").style.display = "block";
     } else {
         showError('Please check your numbers');
     }
+    
 
-    e.preventDefault();
 }
-
 // Show Error
 function showError(error) {
+    // hide laoder
+    document.getElementById("loading").style.display = "none";
+    // show result
+    document.getElementById("results").style.display = "none";
     // Create a div
     const errorDiv = document.createElement('div');
 
